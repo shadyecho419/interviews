5a3e119e7918567bc16fe5a6296cf13ed1a2323d
Russell Mazonde <russellmazonde@gmail.com>
2018-01-10 18:17:04 +0200

    Moved URL to variable incase it needs to be changed, makes changing it centralised instead of changing literals in 2 places. Moved CURL code into function to avoid code duplication. Need to change it to GET instead of post since it appears the request is type GET that is required Moved for loop for getting product price into function to avoid code duplication. Next step is to look through it and optimise it. Will move all literal values into variables. Still going

--- php/ProductController.php
+++ php/ProductController.php
@@ -9,7 +9,7 @@ use Silex\Application;
  * @SLX\Controller(prefix="product/")
  */
 class ProductController {
-
+    private $url = 'http://192.168.0.241/eanlist?type=Web';
     /**
      * @SLX\Route(
      *      @SLX\Request(method="GET", uri="/{id}")
@@ -19,55 +19,52 @@ class ProductController {
      * @return
      */
     public function getById_GET(Application $app, $id){
-        $curl = curl_init();
+        
         $requestData = array();
         $requestData['id'] = $id;
-        curl_setopt($curl, CURLOPT_URL,  'http://192.168.0.241/eanlist?type=Web');
+        $response = execCurlReq($requestData);
+        /*$curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL,  $url);
         curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($curl);
         $response = json_decode($response);
-        curl_close($curl);
-        $result = [];
+        curl_close($curl);*/
+        /*$result = [];
         for ($i =0; $i < count($response) ;$i++) {
             $prod = array();
-            $prod['ean'] =$response[$i]['barcode'];
-            $prod["name"]=$response[$i]['itemName'];
+            $prod['ean'] =$response[$i]['barcode']; //space discrepancy 
+            $prod["name"]=$response[$i]['itemName'];  //space discrepancy 
             $prod["prices"] = array();
             for ($j=0;$j < count($response[$i]['prices']); $j++) {
                 if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
                     $p_price = array();
                     $p_price['price'] = $response[$i]['prices'][$j]['sellingPrice'];
-                    $p_price['curreny'] = $response[$i]['prices'][$j]['currencyCode'];
+                    $p_price['curreny'] = $response[$i]['prices'][$j]['currencyCode']; //mispelt p_price key
                     $prod["prices"][] = $p_price;
                 }
             }
             $result[] = $prod;
-        }
-
+        }*/
+        getProductPrice($response);
         return $app->render('products/product.detail.twig', $result);
     }
-
-    /**
-     * @SLX\Route(
-     *      @SLX\Request(method="GET", uri="/search/{name}")
-     * )
-     * @param Application $app
-     * @param $name
-     * @return
-     */
-    public function getByName_GET(Application $app, $name){
+    
+    private function execCurlReq($requestParm){
+        //change request to use http GET
         $curl = curl_init();
-        $requestData = array();
-        $requestData['names'] = $name;
-        curl_setopt($curl, CURLOPT_URL,  'http://192.168.0.241/eanlist?type=Web');
+        curl_setopt($curl, CURLOPT_URL,  $url);
         curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $requestParm);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($curl);
         $response = json_decode($response);
         curl_close($curl);
+        return $response;
+    }
+    
+    private function getProductPrice($respose){
         $result = [];
         for ($i =0; $i < count($response) ;$i++) {
             $prod = array();
@@ -84,7 +81,47 @@ class ProductController {
             }
             $result[] = $prod;
         }
+        return $result;
+    }
 
+    /**
+     * @SLX\Route(
+     *      @SLX\Request(method="GET", uri="/search/{name}")
+     * )
+     * @param Application $app
+     * @param $name
+     * @return
+     */
+    public function getByName_GET(Application $app, $name){
+        
+        $requestData = array();
+        $requestData['names'] = $name;
+        $response = execCurlReq($requestData);
+        /*$curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL,  'http://192.168.0.241/eanlist?type=Web');
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($curl);
+        $response = json_decode($response);
+        curl_close($curl);*/
+        /*$result = [];
+        for ($i =0; $i < count($response) ;$i++) {
+            $prod = array();
+            $prod['ean'] = $response[$i]['barcode'];
+            $prod["name"]= $response[$i]['itemName'];
+            $prod["prices"] = array();
+            for ($j=0;$j < count($response[$i]['prices']); $j++) {
+                if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
+                    $p_price = array();
+                    $p_price['price'] = $response[$i]['prices'][$j]['sellingPrice'];
+                    $p_price['currency'] = $response[$i]['prices'][$j]['currencyCode'];
+                    $prod["prices"][] = $p_price;
+                }
+            }
+            $result[] = $prod;
+        }*/
+        getProductPrice($response);
         return $app->render('products/products.twig', $result);
     }
 
