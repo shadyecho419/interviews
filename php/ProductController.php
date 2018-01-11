faa9165d6154748ecd6a1dfa3a0d55ffb06439d9
Russell Mazonde <russellmazonde@gmail.com>
2018-01-11 08:32:47 +0200

    Added debug statements incase the script ever has to be debugged and execution state wants to be tracked. Added json handling for data so that data requests have uniformity Moved loop for getting product price into a function to prevent duplication CURL request in function changed to GET instead of POST since it seemed the request type is GET

--- php/ProductController.php
+++ php/ProductController.php
@@ -9,7 +9,9 @@ use Silex\Application;
  * @SLX\Controller(prefix="product/")
  */
 class ProductController {
-    private $url = 'http://192.168.0.241/eanlist?type=Web';
+    private $baseUrl = 'http://192.168.0.241/eanlist?type=Web';
+    private $debug = 0; //enable debug mode by setting to 1 so debug statements can print out
+
     /**
      * @SLX\Route(
      *      @SLX\Request(method="GET", uri="/{id}")
@@ -19,68 +21,115 @@ class ProductController {
      * @return
      */
     public function getById_GET(Application $app, $id){
+        if($this->debug > 0)
+            echo __METHOD__ . " starting now " . "<br/>";
+            
+        if(!isset($id)){
+            $response["error"] = TRUE;
+            $response["error_msg"] = "ID Parameter not set";
+            $response = json_encode($response);
+            return $response;
+        }
         
-        $requestData = array();
-        $requestData['id'] = $id;
-        $response = execCurlReq($requestData);
-        /*$curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL,  $url);
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($curl);
-        $response = json_decode($response);
-        curl_close($curl);*/
-        /*$result = [];
-        for ($i =0; $i < count($response) ;$i++) {
-            $prod = array();
-            $prod['ean'] =$response[$i]['barcode']; //space discrepancy 
-            $prod["name"]=$response[$i]['itemName'];  //space discrepancy 
-            $prod["prices"] = array();
-            for ($j=0;$j < count($response[$i]['prices']); $j++) {
-                if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
-                    $p_price = array();
-                    $p_price['price'] = $response[$i]['prices'][$j]['sellingPrice'];
-                    $p_price['curreny'] = $response[$i]['prices'][$j]['currencyCode']; //mispelt p_price key
-                    $prod["prices"][] = $p_price;
-                }
-            }
-            $result[] = $prod;
-        }*/
-        getProductPrice($response);
-        return $app->render('products/product.detail.twig', $result);
+        if(!isset($app)){
+            $response["error"] = TRUE;
+            $response["error_msg"] = "App parameter not set";
+            $response = json_encode($response);
+            return $response;
+        }
+        
+        $requestUrl = $baseUrl . "&id=$id";
+        if($this->debug > 0)
+            echo __METHOD__ . " Request URL: $requestUrl" . "<br/>";
+            
+        $response = execCurlReq($requestUrl);
+        if($this->debug > 0)
+            echo __METHOD__ . " CURL request done" . "<br/>";
+        
+        if(!$response["error"]){
+            $result = getProductPrice($response["result"]);
+            $result = json_encode($result);
+            
+            if($this->debug > 0)
+                echo __METHOD__ . " Returning successful request results: $result" . "<br/>";
+            
+            return $app->render('products/product.detail.twig', $result);
+        }
+        
+        $response = json_encode($response);
+        if($this->debug > 0)
+            echo __METHOD__ . " Returning request result due to error: $response" . "<br/>";
+                
+        return $app->render('products/product.detail.twig', $response);
     }
     
     private function execCurlReq($requestParm){
-        //change request to use http GET
+        if($this->debug > 0)
+            echo __METHOD__ . " starting now with parameter $requestParm" . "<br/>";
+            
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL,  $url);
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $requestParm);
+        curl_setopt($curl, CURLOPT_URL,  $requestParm);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($curl);
-        $response = json_decode($response);
+        
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        $result = curl_exec($curl);
+        
+        $result = json_decode($result);
         curl_close($curl);
+        
+        if(curl_errno($curl)){
+            $response["error"] = TRUE;
+            $response["error_msg"] = "Error performing request: " . curl_error($curl);
+            if($this->debug > 0)
+                echo __METHOD__ . " CURL request error: " . curl_error($curl) . "<br/>";
+            
+            return $response;
+        }
+        
+        $response["error"] = FALSE;
+        $response["result"] = $result;
+        if($this->debug > 0)
+                echo __METHOD__ . " CURL request result: " . json_encode($response) . "<br/>";
+        
         return $response;
     }
     
-    private function getProductPrice($respose){
-        $result = [];
-        for ($i =0; $i < count($response) ;$i++) {
+    /*
+     * get price from response. Parse response from 
+     */
+    function getProductPrice($response){
+        if($this->debug > 0)
+            echo __METHOD__ . " starting now with parameter" . "<br/>";
+            
+        $price = [];
+        $i_size = count($response);
+        for ($i =0; $i < $i_size ;$i++) {
             $prod = array();
             $prod['ean'] = $response[$i]['barcode'];
             $prod["name"]= $response[$i]['itemName'];
+
             $prod["prices"] = array();
-            for ($j=0;$j < count($response[$i]['prices']); $j++) {
-                if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
-                    $p_price = array();
-                    $p_price['price'] = $response[$i]['prices'][$j]['sellingPrice'];
-                    $p_price['currency'] = $response[$i]['prices'][$j]['currencyCode'];
+            $j_size = count($response[$i]['prices']);
+
+            for ($j=0;$j < $j_size; $j++) {
+                $p_price = array();
+                $p_responsePrices = $response[$i]['prices'][$j];
+                
+                if ($p_responsePrices['currencyCode'] != 'ZAR') {    
+                    $p_price['price'] = $p_responsePrices['sellingPrice'];
+                    $p_price['currency'] = $p_responsePrices['currencyCode'];
                     $prod["prices"][] = $p_price;
                 }
             }
-            $result[] = $prod;
+            $price[] = $prod;
         }
+        
+        $result["error"] = FALSE;
+        $result["result"] = $price;
+        if($this->debug > 0)
+            echo __METHOD__ . " CURL request result: " . json_encode($result) . "<br/>";
+                
         return $result;
     }
 
@@ -93,36 +142,46 @@ class ProductController {
      * @return
      */
     public function getByName_GET(Application $app, $name){
+        if($this->debug > 0)
+            echo __METHOD__ . " starting now " . "<br/>";
+            
+        if(!isset($name)){
+            $response["error"] = TRUE;
+            $response["error_msg"] = "Name Parameter not set";
+            $response = json_encode($response);
+            return $response;
+        }
         
-        $requestData = array();
-        $requestData['names'] = $name;
-        $response = execCurlReq($requestData);
-        /*$curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL,  'http://192.168.0.241/eanlist?type=Web');
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($curl);
-        $response = json_decode($response);
-        curl_close($curl);*/
-        /*$result = [];
-        for ($i =0; $i < count($response) ;$i++) {
-            $prod = array();
-            $prod['ean'] = $response[$i]['barcode'];
-            $prod["name"]= $response[$i]['itemName'];
-            $prod["prices"] = array();
-            for ($j=0;$j < count($response[$i]['prices']); $j++) {
-                if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
-                    $p_price = array();
-                    $p_price['price'] = $response[$i]['prices'][$j]['sellingPrice'];
-                    $p_price['currency'] = $response[$i]['prices'][$j]['currencyCode'];
-                    $prod["prices"][] = $p_price;
-                }
-            }
-            $result[] = $prod;
-        }*/
-        getProductPrice($response);
-        return $app->render('products/products.twig', $result);
+        if(!isset($app)){
+            $response["error"] = TRUE;
+            $response["error_msg"] = "App parameter not set";
+            $response = json_encode($response);
+            return $response;
+        }
+        
+        $requestUrl = $baseUrl . "&names=$name";
+        if($this->debug > 0)
+            echo __METHOD__ . " Request URL: $requestUrl" . "<br/>";
+            
+        $response = execCurlReq($requestUrl);
+        if($this->debug > 0)
+            echo __METHOD__ . " CURL request done" . "<br/>";
+        
+        if(!$response["error"]){
+            $result = getProductPrice($response["result"]);
+            $result = json_encode($result);
+            
+            if($this->debug > 0)
+                echo __METHOD__ . " Returning successful request results: $result" . "<br/>";
+                
+            return $app->render('products/products.twig', $result);
+        }
+        
+        $response = json_encode($response);
+        if($this->debug > 0)
+                echo __METHOD__ . " Returning request result due to error: $response" . "<br/>";
+                
+        return $app->render('products/product.detail.twig', $response);
     }
 
 }
